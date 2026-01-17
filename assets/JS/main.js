5f31a8a05a290a763a1e2465bca2c5df65bfa9a6
Sofi VZ <sv.zuccar@gmail.com>
2026-01-17 18:24:25 -0300

    asignando atributos al constructor

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,3 +1,35 @@
 class Pokemon {
-    
+    constructor(data){
+        this.nombre=data.name;
+        this.id=data.id;
+        this.sprites=data.sprites;
+        this.types=data.types;
+        this.height=data.height;
+        this.weight=data.weight;
+        this.stats=data.stats;
+        this.isDefault
+        this.order
+        this.abilities
+        this.moves
+        this.baseExperience
+    }
+    get imagen(){
+        return this.sprites.other['official-artwork'].front_default
+    }
+    get tipos(){
+        const listaTipos=this.types.map(item=>item.type.name);
+        return `${listaTipos.join(', ')}.`
+    }
+    get altura(){
+        return `${(this.height/10).toFixed(1)} kg.`
+    }
+    get peso(){
+        return `${(this.weight/10).toFixed(1)} kg.`
+    }
+    get estadisticas(){
+        this.stats.reduce((acc,item)=>{
+            acc[item.stat.name] = item.base_stat;
+            return acc;
+        }, {});
+    }
 }
