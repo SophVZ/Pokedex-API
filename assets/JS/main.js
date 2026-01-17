b19d525ce869a0dde498a42d4e3a9cf7a344462c
Sofi VZ <sv.zuccar@gmail.com>
2026-01-17 18:54:09 -0300

    últimos get de estadística

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -14,26 +14,44 @@ class Pokemon {
         this.order=data.order;
         //avanzadas
         this.abilities=data.abilities;
-        this.moves
+        this.moves=data.moves;
     }
     get imagen(){
-        return this.sprites.other['official-artwork'].front_default
+        return this.sprites.other['official-artwork'].front_default || this.sprites.front_default
     }
     get tipos(){
         const listaTipos=this.types.map(item=>item.type.name);
         return `${listaTipos.join(', ')}.`
     }
     get altura(){
-        return `${(this.height/10).toFixed(1)} kg.`
+        return `${(this.height/10).toFixed(1)} m.`
     }
     get peso(){
         return `${(this.weight/10).toFixed(1)} kg.`
     }
     get estadisticas(){
-        this.stats.reduce((acc,item)=>{
+        return this.stats.reduce((acc,item)=>{
             acc[item.stat.name] = item.base_stat;
             return acc;
         }, {});
+    }
+    get hp(){
+        return `${this.estadisticas.hp}`
+    }
+    get ataque(){
+        return `${this.estadisticas.attack}`
+    }
+    get defensa(){
+        return `${this.estadisticas.defense}`
+    }
+    get ataqueEspecial(){
+        return `${this.estadisticas[special-attack]}`
+    }
+    get defensaEspecial(){
+        return `${this.estadisticas[special-defense]}`
+    }
+    get velocidad(){
+        return `${this.estadisticas.speed}`
     }
     get habilidades(){
         const listaAbilities=this.abilities.map(a=>a.ability.name);
